1e7c2a9f0bab3068c8dde658586dea00788c2779
Michaël Zasso <targos@protonmail.com>
2017-03-16 15:20:10 +0100

    add link to code in build

--- scripts/createIndex.js
+++ scripts/createIndex.js
@@ -3,13 +3,13 @@
 const fs = require('fs');
 const path = require('path');
 const dir = path.join(__dirname, '../build');
-const dirs = [];
+
 fs.readdir(dir, function (err, files) {
     if (err) {
         console.error(err);
         process.exit(1);
     }
-    files.filter(file => fs.statSync(path.join(dir, file)).isDirectory());
+    files = files.filter(file => fs.statSync(path.join(dir, file)).isDirectory());
     const indexFile = `
             <!DOCTYPE html>
             <html lang="en">
@@ -18,7 +18,7 @@ fs.readdir(dir, function (err, files) {
                 <title>Index</title>
             </head>
             <body>
-            ${files.map(file => `<h2><a href="${file}">${file}</a></h2><br/>`).join('\n')}
+            ${files.map(file => `<h4><a href="${file}">${file}</a> (<a target="_blank" href="https://github.com/neptunjs/experiments/blob/master/src/${file}/app.js">Code</a>)</h4><br/>`).join('\n')}
             <script src="./bundle.js"></script>
             </body>
             </html>
